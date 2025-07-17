afd01d9caed81ce3720e9a2f5e92ff43046da27a
Harshjethwa07 <jethwaharsh26@gmail.com>
2025-07-17 22:19:47 +0530

    Add HTML file to include JavaScript program and display output

--- basic-2.js
+++ basic-2.js
@@ -1,6 +1,12 @@
-// add the two numbers
+let a = 10;
+let b = 20;
+console.log("the sum of a and b is " + (a+b));
 
-let a = 3;
-let b = 4;
-let c = a + b;
-console.log(`the sum of ${a} and ${b} is ${c}`);
+
+
+
+
+
+
+
+
