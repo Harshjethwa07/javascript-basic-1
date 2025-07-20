ac9a0dd880ea9823714d2c2f30babae7835b37b2
Harshjethwa07 <jethwaharsh26@gmail.com>
2025-07-20 21:35:21 +0530

    Add basic variable swapping program and update script reference in HTML

---
+++ basic-3.js
@@ -0,0 +1,30 @@
+// let age = Number(prompt("enter your age"));
+// console.log(age); 
+
+
+
+
+//swap two variables via 3 method 
+
+//first method
+let a = 10;
+let b = 20;
+let c;
+
+c = a;// a=10,c= 10;
+a = b;//a=20,b=20;
+b = c;//b=10,c=20
+
+console.log(a);
+console.log(b);
+
+
+
+//second method 
+
+
+
+
+
+
+
