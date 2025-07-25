af9b78e5fd8de4647befb3d8990cecf31f6dc31c
Harshjethwa07 <jethwaharsh26@gmail.com>
2025-07-25 08:33:09 +0530

    Comment out unary operator examples for clarity

--- basic(UO)-5.js
+++ basic(UO)-5.js
@@ -1,6 +1,13 @@
 //Unary Operators in javascrpt ++ -- 
 
-let a = 10;
-let b = ++ a;
-console.log(b);
+// let a = 10;
+// let b = ++ a;
+// console.log(b);
 
+
+
+
+
+let i=11;
+i = i++ + ++i;
+console.log(i);
