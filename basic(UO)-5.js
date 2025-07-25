bd1a09036b2e322abbaabc48ad75907505c66018
Harshjethwa07 <jethwaharsh26@gmail.com>
2025-07-25 08:56:17 +0530

    Refactor: Comment out example code for clarity and remove unused variable calculations

--- basic(UO)-5.js
+++ basic(UO)-5.js
@@ -4,9 +4,21 @@
 // let b = ++ a;
 // console.log(b);
 
+//queston1 its solved 
+// let i=11;
+// i = i++ + ++i;
+// console.log(i);
 
-let i=11;
-i = i++ + ++i;
-console.log(i);
+
+//question 2 sloved
+
+let a = 11;
+let b = 22;
+let c = a + b + a++ + b++ + ++a + ++b;
+console.log("a=" + a);
+console.log("b=" + b);
+console.log("c=" + c);
+
+
 
 
